be1977892b27ec3c2ec8c5341ab0a2b460ec7fba
Landry Rutalindwa <rut.landry@gmail.com>
2023-05-19 12:04:47 +0200

    Refactoring in progress.

--- src/main/java/com/example/shiftplaner/model/Mitarbeiter.java
+++ src/main/java/com/example/shiftplaner/model/Mitarbeiter.java
@@ -1,16 +1,18 @@
+//Package name
 package com.example.shiftplaner.model;
-
+//Diese Klasse dient zur Erstellung der Mitarbeiter-Objekte
 public class Mitarbeiter {
+    //Variablen für die Mitarbeiter-Objekte
     private String name;
-
+    //Konstruktor für die Mitarbeiter-Objekte
     public Mitarbeiter(String name) {
         this.name = name;
     }
-
+    //Getter und Setter für die Variablen der Mitarbeiter-Objekte
     public String getName() {
         return name;
     }
-
+    //Getter und Setter für die Variablen der Mitarbeiter-Objekte
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/example/shiftplaner/model/Schicht.java
+++ src/main/java/com/example/shiftplaner/model/Schicht.java
@@ -1,23 +1,27 @@
+//Package name
 package com.example.shiftplaner.model;
-
+//Diese Klasse dient zur Erstellung der Schicht-Objekte
 public class Schicht {
+    //Variablen für die Schicht-Objekte
     private String name;
+    //Konstruktor für die Schicht-Objekte
     private String zeit;
-
+    //Konstruktor für die Schicht-Objekte
     public Schicht(String name) {
         this.name = name;
         this.zeit = zeit;
     }
-
+    //Getter und Setter für die Variablen der Schicht-Objekte
     public String getName() {
         return name;
     }
-
+    //Getter und Setter für die Variablen der Schicht-Objekte
     public String getZeit() {
         return zeit;
     }
 
     @Override
+    //Methode zur Ausgabe der Schicht-Objekte
     public String toString() {
         return name + "\n" + zeit;
     }
